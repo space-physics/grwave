9353706200d6c074291ca0503d1f2ea95cd9894c
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-12-18 00:15:55 -0500

    init

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+install_requires=[]
+tests_require=['nose','coveralls']
+# %%
+from setuptools import setup,find_packages
+
+setup(name='pygrwave',
+      packages=find_packages(),
+      description='Groundwave per ITU P.368 10 kHz - 30 MHz',
+      version='0.1.0',
+      url='https://github.com/scivision/python-grwave',
+      classifiers=[
+      'Intended Audience :: Science/Research',
+      'Development Status :: 3 - Alpha',
+      'Programming Language :: Python :: 3',],
+      install_requires=install_requires,
+      tests_require=tests_require,
+      extras_require={'tests':tests_require,
+                      'plot':['matplotlib','seaborn',],},
+      python_requires='>=3.6',
+   )
